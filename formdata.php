d4d8fd75667a2ba3ac86fcb7d097f7b95a4d427e
Project 1 <Paladin@MacBook-Air-Andrii.local>
2018-04-19 08:59:43 +0300

    fixed form

--- formdata.php
+++ formdata.php
@@ -1,10 +1,32 @@
-﻿<?php 
-	$result = mail("palitra.mebeli.od@gmail.com", "Форма с сайта", "С сайта была получена анкета с такими данными: \nИмя: $_POST[name] \nНомер телефона: $_POST[tel]");
-	if ($result) {
-		echo "<p>Сообщение отправлено успешно!</p>";
-	}
-
-	else {
-		echo "<p>Сообщение  НЕ отправлено!</p>";
-	}
+﻿<?php
+ 
+/* Задаем переменные */
+$name = htmlspecialchars($_POST["name"]);
+$tel = htmlspecialchars($_POST["tel"]);
+$bezspama = htmlspecialchars($_POST["bezspama"]);
+ 
+/* Ваш адрес и тема сообщения */
+$address = "palitra.mebeli.od@gmail.com";
+$sub = "Сообщение с моего сайта";
+ 
+/* Формат письма */
+$mes = "Сообщение с моего сайта.\n
+Имя отправителя: $name 
+Телефон отправителя: $tel";
+ 
+ 
+if (empty($bezspama)) /* Оценка поля bezspama - должно быть пустым*/
+{
+/* Отправляем сообщение, используя mail() функцию */
+$from  = "From: $name <$tel> \r\n";
+if (mail($address, $sub, $mes, $from)) { 
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Заказ отправлен! Оставайтесь на связи.</body>';}
+else {
+ echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Что-то пошло не так. Попробуйте ещё раз.</body>';}
+}
+exit; /* Выход без сообщения, если поле bezspama заполнено спам ботами */
 ?>
